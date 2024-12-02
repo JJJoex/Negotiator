2ab66f86a03427a4e740e4c0493dcb2430e80108
Joe Xiong <1102871771@qq.com>
2024-12-02 16:05:55 +0800

    页面框架

---
+++ Negotiator/src/components/steps.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-steps style="" :active="1" align-center>
+    <el-step title="Description"  />
+    <el-step title="Preparation"  />
+    <el-step title="Negotiation"  />
+    <el-step title="Finish"  />
+  </el-steps>
+</template>
