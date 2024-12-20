9c3302715df499bb694c835a04a4e1cbc0d01390
Joe Xiong <1102871771@qq.com>
2024-12-20 23:49:08 +0800

    新增确认页

--- Negotiator/src/router.js
+++ Negotiator/src/router.js
@@ -3,10 +3,12 @@ import Introduction from "./pages/Introduction.vue"
 import Negotiation from "./pages/Negotiation.vue"
 import Finish from "./pages/Finish.vue"
 import Preparation from "./pages/Preparation.vue"
+import Ensurement from "./pages/Ensurement.vue"
 
 const routes = [
     {path: '/description', component: Introduction},
     {path: '/preparation', component: Preparation},
+    {path: '/ensurement', component: Ensurement},
     {path: '/negotiation', component: Negotiation},
     {path: '/finish', component: Finish},
     { path: '/', redirect: '/description' }, 
